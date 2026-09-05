94cb31e4464d0a054393c5219a7f48577df74f60
sashagreench-droid <sashagreench@gmail.com>
2026-09-05 11:52:45 +0700

    Make Day 49 the true course finale

--- production.py
+++ production.py
@@ -31,6 +31,7 @@ try:
     runpy.run_module("day_feedback_patch", run_name="__main__")
     runpy.run_module("day48_practice_patch", run_name="__main__")
     runpy.run_module("day49_practice_patch", run_name="__main__")
+    runpy.run_module("day49_final_patch", run_name="__main__")
     runpy.run_module("day30_debug_patch", run_name="__main__")
 finally:
     bot.main = _real_main
